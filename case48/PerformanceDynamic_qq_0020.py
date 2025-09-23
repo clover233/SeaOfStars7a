b2bd783a0f55ecee0a3ce6bb63ccdc3bb04301c6
fang <415984774@qq.com>
2025-09-23 11:17:20 +0800

    新增动态模型basic用例执行顺序

--- case48/PerformanceDynamic_qq_0020.py
+++ case48/PerformanceDynamic_qq_0020.py
@@ -6,7 +6,7 @@ from aw import SeaOfStarsAW
 from cases.CaseBase import Case
 
 
-class PerformanceDynamic_qq_0010(Case):
+class PerformanceDynamic_qq_0020(Case):
     all_app_package_list = ['']
     TEST_TIME = 1
 
