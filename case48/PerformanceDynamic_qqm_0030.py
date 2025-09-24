2ddf66f0ec415a66983306cab866260769962180
clover233 <1207333199@qq.com>
2025-09-24 15:17:53 +0800

    # 用例刷新0924-1515

--- case48/PerformanceDynamic_qqm_0030.py
+++ case48/PerformanceDynamic_qqm_0030.py
@@ -40,9 +40,9 @@ class PerformanceDynamic_qqm_0030(Case):
 
             logging.info('启动QQ音乐')
             # SeaOfStarsAW.trace_thread.add_log('QQ音乐', '启动QQ音乐')
-            SeaOfStarsAW.ut_device.swipe_left()
-            SeaOfStarsAW.ut_device.click(0.847, 0.135)
-
+            # SeaOfStarsAW.ut_device.swipe_left()
+            # SeaOfStarsAW.ut_device.click(0.847, 0.135)
+            SeaOfStarsAW.ut_device.session().app_activate("com.tencent.QQMusic")
             time.sleep(8)
             logging.info('上滑2次，下滑2次')
             for _ in range(2):
@@ -62,7 +62,8 @@ class PerformanceDynamic_qqm_0030(Case):
             time.sleep(2)
             logging.info('启动微博')
             # SeaOfStarsAW.trace_thread.add_log('微博', '启动微博')
-            SeaOfStarsAW.ut_device.click(0.846, 0.125)
+            # SeaOfStarsAW.ut_device.click(0.846, 0.125)
+            SeaOfStarsAW.ut_device.session().app_activate("com.sina.weibo")
             time.sleep(8)
             logging.info('上滑5次，下滑5次')
             for _ in range(5):
@@ -96,7 +97,7 @@ class PerformanceDynamic_qqm_0030(Case):
                 SeaOfStarsAW.ut_device.swipe_down()
                 time.sleep(2)
             logging.info('点击返回')
-            SeaOfStarsAW.ut_device(label='返回').click()
+            SeaOfStarsAW.ut_device.swipe(0.010, 0.809, 0.933, 0.805, 0.5)
             time.sleep(2)
             # SeaOfStarsAW.trace_thread.add_log('微博', '上滑退出')
             logging.info('上滑退出')
@@ -116,7 +117,8 @@ class PerformanceDynamic_qqm_0030(Case):
             time.sleep(2)
             logging.info('启动京东')
             # SeaOfStarsAW.trace_thread.add_log('京东', '启动京东')
-            SeaOfStarsAW.ut_device.click(0.156, 0.244)
+            # SeaOfStarsAW.ut_device.click(0.156, 0.244)
+            SeaOfStarsAW.ut_device.session().app_activate("com.360buy.jdmobile")
             time.sleep(5)
             logging.info('上滑5次，下滑5次')
             for _ in range(5):
--- case48/PerformanceDynamic_qunaer_0010.py
+++ case48/PerformanceDynamic_qunaer_0010.py
@@ -42,6 +42,7 @@ class PerformanceDynamic_qunaer_0010(Case):
             # SeaOfStarsAW.trace_thread.add_log('去哪旅行', '去哪旅行')
             SeaOfStarsAW.ut_device.swipe_left()
             SeaOfStarsAW.ut_device.click(0.852, 0.259)
+            # SeaOfStarsAW.ut_device.app_activate("com.qunar.iphoneclient8")
 
             time.sleep(2)
             logging.info('点击机票')
